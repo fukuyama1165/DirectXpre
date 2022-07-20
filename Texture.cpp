70dc0735108499fa4fc922f1ba0e535932e16196
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2022-07-20 10:55:48 +0900

    テクスチャの変えるの忘れてた

--- Texture.cpp
+++ Texture.cpp
@@ -33,8 +33,8 @@ void Texture::Draw(ID3D12GraphicsCommandList* cmdList)
 	//SRVヒープの先頭にあるSRVをルートパラメータ２番に設定
 	cmdList->SetGraphicsRootDescriptorTable(2, srvGpuHandle);
 
-	/*srvGpuHandle.ptr += incremantSize;
-	cmdList->SetGraphicsRootDescriptorTable(2, srvGpuHandle);*/
+	srvGpuHandle.ptr += incremantSize;
+	cmdList->SetGraphicsRootDescriptorTable(2, srvGpuHandle);
 
 }
 
