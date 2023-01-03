ddbf665d2df775541d952f56a255312ef7b0e83e
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2023-01-03 15:40:44 +0900

    シェーダーと頂点数減らした

--- Resources/shaders/BasicPS.hlsl
+++ Resources/shaders/BasicPS.hlsl
@@ -8,8 +8,9 @@ float4 main(VSOutput input) : SV_TARGET
 {
 	float4 texcolor = float4(tex.Sample(smp,input.uv));
 
-	float3 light = normalize(float3(1,-1,1));
+	/*float3 light = normalize(float3(1,-1,1));
 	float diffuse = saturate(dot(-light, input.normal));
 	float brightness = diffuse + 0.3f;
-	return float4(texcolor.rgb * brightness, texcolor.a);
+	return float4(texcolor.rgb * brightness, texcolor.a);*/
+	return input.color * texcolor;
 }
--- Resources/shaders/BasicVS.hlsl
+++ Resources/shaders/BasicVS.hlsl
@@ -2,9 +2,34 @@
 
 VSOutput main( float4 pos : POSITION,float3 normal:NORMAL, float2 uv:TEXCOORD)
 {
+	//VSOutput output;//ピクセルシェーダーに渡す値
+	//output.svpos = mul(mat,pos);
+	//output.normal = normal;
+	//output.uv = uv;
+	//return output;
+
+	float3 light = normalize(float3(1, -1, 1));
+
+	float3 lightcolor = float3(1, 1, 1);
+
+	float3 ambient = 1;
+
+	float3 diffuse = dot(-light, normal);
+
+	const float3 eye = float3(0, 0, -20);
+
+	const float shininess = 4.0f;
+
+	float3 eyedir = normalize(eye - pos.xyz);
+
+	float3 reflect = normalize(-light + 2 * dot(light, normal) * normal);
+
+	float3 specular = pow(saturate(dot(reflect, eyedir)), shininess) * 1;
+
 	VSOutput output;//ピクセルシェーダーに渡す値
-	output.svpos = mul(mat,pos);
-	output.normal = normal;
+	output.svpos = mul(mat, pos);
+	output.color.rgb = (ambient + diffuse + specular) * lightcolor;
+	output.color.a = 1;
 	output.uv = uv;
 	return output;
 }
