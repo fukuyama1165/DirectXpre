da497f5524ec4d22249948cd99fd501dcdc68490
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2023-04-20 11:37:01 +0900

    導入

---
+++ assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/k021g1165/DirectX/DirectXpre/assimp/contrib/zlib
+# Build directory: C:/k021g1165/DirectX/DirectXpre/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
